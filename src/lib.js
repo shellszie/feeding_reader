d10c99c98e09e9890a3927a3115f9aea9e306faf
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-11-24 17:18:24 -0800

    adding print statement for debugging on heroku

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,11 @@
 // const axios = require('axios');
 
 export const baseUrl = () => {
-
     if (process.env.NODE_ENV === 'development') {
+        console.log("process.env.NODE_ENV = " + process.env.NODE_ENV);
         return "http://localhost:5000";
     } else {
+        console.log("process.env.NODE_ENV = " + process.env.NODE_ENV);
         return "https://intense-beyond-88293-6a4d48fa1f58.herokuapp.com";
     }
 }
