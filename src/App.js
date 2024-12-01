9d5994738c4a0050dc318a40f60c84c25c32feb3
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-12-01 09:00:10 -0800

    added route and new page for sign up form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Books from './Books.js';
 import Image from 'react-bootstrap/Image';
+import { BrowserRouter, Routes, Route } from "react-router";
+import Signup from './Signup.js';
 
 function App() {
     return (
         <div className="App">
-            <Image src="logo.png" className="logo" thumbnail />
+
             <span className="feeding-reader">Feeding Reader</span>
-            <Books/>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Books />} />
+                    <Route path="/signup" element={<Signup />}>
+                        {/*<Route index element={<RecentActivity />} />*/}
+                        {/*<Route path="project/:id" element={<Project />} />*/}
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
