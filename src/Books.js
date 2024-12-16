a83b21c97f42be4c47d7b0c616e3fff56a022d1d
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-12-16 10:16:22 -0800

    margin for welcome msg

--- src/Books.js
+++ src/Books.js
@@ -55,7 +55,7 @@ export default function Books() {
         <>
             <div className="heading-wrapper">
                 <div className="feeding-reader">Feeding Reader</div>
-                <div>
+                <div className="mt-3">
                     <div>Welcome {localStorage.getItem('email')}</div>
                     <Logout/>
                 </div>
