54a1f9182c7bc9be583c5c74a2bf082eaf80f2be
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2024-12-16 08:20:42 -0800

    authorization for protected books page is working. only logged in users can view the homepage feed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import Books from './Books.js';
 import { BrowserRouter, Routes, Route } from "react-router";
 import Signup from './Signup.js';
 import Login from './Login.js';
+import {Navigate, Outlet} from 'react-router';
+
+
 
 function App() {
+
+    const ProtectedRoute = ({
+                                jwt,
+                                redirectPath = '/login',
+                                children,
+                            }) => {
+        if (!jwt) {
+            return <Navigate to={redirectPath} replace />;
+        }
+
+        return children ? children : <Outlet />;
+    };
+
     return (
         <div className="App">
 
@@ -12,9 +28,17 @@ function App() {
             <span>Welcome {localStorage.getItem('email')}</span>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Books />} />
+                    {/*<Route path="/" element={<Books />} />*/}
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoute jwt={localStorage.getItem('jwt')}>
+                                <Books />
+                            </ProtectedRoute>
+                        }
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
