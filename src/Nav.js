8ac485fa0ed463d2c0c875b62f603315c91b7483
Michelle Kam <169413419+shellszie@users.noreply.github.com>
2025-01-05 13:47:27 -0800

    Feeding Reader title stays together on one line on small screens

--- src/Nav.js
+++ src/Nav.js
@@ -2,13 +2,13 @@ import Logout from "./Logout";
 import React from "react";
 
 function Nav() {
-
-
     return (
         <div className="heading-wrapper mb-3">
             <div className="feeding-reader">
-                <img alt="Feeding Reader Book Previews" src="logo_50x50.png" className="logo"/>&nbsp;
-                <span>Feeding Reader</span>
+                <img alt="Feeding Reader Book Previews" src="logo_50x50.png" className="logo mt-3" />&nbsp;
+                <span className="logo-title">
+                    <span>Feeding</span>&nbsp;<span>Reader</span>
+                </span>
             </div>
 
             <div className="mt-3">
